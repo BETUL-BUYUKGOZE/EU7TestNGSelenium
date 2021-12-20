7d8b552707ea589f327988f5da8c6cb4c10d63bd
BETUL-BUYUKGOZE <96446947+BETUL-BUYUKGOZE@users.noreply.github.com>
2021-12-20 22:44:28 +0100

    Testing (#1)
    * fixing the test case.d
    
    * fixing the test case.e
    
    * Update AmazonTest.java
    
    new line
    
    Co-authored-by: EduPen <zekeriya.bdtr@outlook.com>

--- src/test/java/com/cydeo/tests/day5_xpath/AmazonTest.java
+++ src/test/java/com/cydeo/tests/day5_xpath/AmazonTest.java
@@ -42,6 +42,13 @@ public class AmazonTest {
 
         driver.quit();
 
+
+        System.out.println("1");
+        System.out.println("2");
+         System.out.println("3");
+        
+
+
     }
 
 
